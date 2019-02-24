24c2fbedfd963eccccce3091bc10ea88a05cceeb
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-24 03:17:53 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -58,6 +58,7 @@ def shortened_tweet_truncator(tweet)
       tweet
     end
   end
+  puts tweet.length
 end
 
 puts shortened_tweet_truncator("GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!")
